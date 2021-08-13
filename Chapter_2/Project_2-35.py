d83cd0c382a598972a599fbf128b411fb2ffa7db
Andres <nanoandres_24@hotmail.com>
2021-08-12 20:47:50 -0500

    Testing private variable behavior

--- Chapter_2/Project_2-35.py
+++ Chapter_2/Project_2-35.py
@@ -8,7 +8,44 @@ them to Bob's computer, and Bob is periodically
 checking if his computer has a packet from Alice, and, if
 so, he reads and deletes it.
 """
-
+# packet --> user_name - ip_address - email
 class Packets:
-    def __int__():
-        #TODO: Define packets' traits
+    def __int__(self, username="None", ipaddress="None", email="None"):
+        self._username = username
+        self._ipaddress = ipaddress
+        self._email = email
+
+    @property
+    def name(self):
+        return self._username
+
+    @property
+    def ip_adr(self):
+        return self._ipaddress
+
+    @property
+    def email(self):
+        return self._email
+
+    @name.setter
+    def name(self, uname):
+        self._username = uname
+
+    @ip_adr.setter
+    def ip_adr(self, newIP):
+        self._ipaddress = newIP
+
+    @email.setter
+    def email(self, newEmail):
+        self._email = newEmail
+
+    def __str__(self):
+        return f"User_name: {self.username} {self._username}, IP address {self.ipaddress} {self._ipaddress}, email {self.email} {self._email}"
+        #return f"User: {self._username}, IP address {self._ipaddress}, email {self._email}"
+        #return f"User: {self.username}, IP address {self.ipaddress}, email {self.email}"
+
+tito = Packets()
+tito.username = "TITO"
+tito.ipaddress = "182.784.986"
+tito.email = "tito@cats.com"
+print(tito)
