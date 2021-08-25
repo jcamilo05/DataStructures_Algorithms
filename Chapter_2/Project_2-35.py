55f6f8c604d5434040b997ffad1d87bfbcde62e5
Juan <camilo.giar@gmail.com>
2021-08-25 07:45:48 -0500

    modify Project_2-35 and add gitignore

--- Chapter_2/Project_2-35.py
+++ Chapter_2/Project_2-35.py
@@ -8,45 +8,38 @@ them to Bob's computer, and Bob is periodically
 checking if his computer has a packet from Alice, and, if
 so, he reads and deletes it.
 """
-# packet --> user_name - ip_address - email
-#TODO: Test setters and getters
-class Packets:
+class User:
+    """
+    This class receive as paremeter a username, ip address and a email to create 
+    a user
+    """
     def __init__(self, username="None", ipaddress="None", email_adr="None"):
         self._name = username
         self._ipaddr = ipaddress
         self._email = email_adr
 
-    @property
+    @property            #getter
     def name(self):
-        print("calling name getter")
         return self._name
 
-    @property
+    @property            #getter
     def ipaddr(self):
-        print("calling ipaddr getter")
         return self._ipaddr
 
-    @property
+    @property            #getter
     def email(self):
-        print("calling email getter")
         return self._email
 
     @name.setter
     def name(self, uname):
-        print("calling name setter")
         self._name = uname
 
     @ipaddr.setter
     def ipaddr(self, newIP):
-        print("calling ipadr setter")
-        if not isinstance(newIP, int):
-            self._ipaddr = newIP
-        else:
-            self._ipaddr = 0
+        self._ipaddr = newIP
 
     @email.setter
     def email(self, newEmail):
-        print("calling email setter")
         self._email = newEmail
 
     def __str__(self):
@@ -55,36 +48,32 @@ class Packets:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.ipaddr, self.email)
 
-class GenPackets(Packets):  
+class Deliver:
+    
+    """
+        This Class take as a parameter a user which is a object created with User class
+        and return a delivered message to another user object that must be given as a 
+        parameter in send method.
 
-    # To review later
-    #@Packets.name.setter
-    #@Packets.email.setter
-    #@Packets.ip_adr.setter 
-    #def overall_setter(self, names, emails, ips):
-    #    #TODO unpack with zip(*data)
-    #    Packets.name.fset(self, names)
-    #    Packets.email.fset(self, emails)
-    #    Packets.ip_adr.fset(self, ips)
-    def unpack_data(self, *data):
-        counter = 0
-        self.name = tuple(zip(*data))[0][0]
-        self.email = tuple(zip(*data))[1][0]
-        self.ipaddr = tuple(zip(*data))[2][0]
-        #for i in zip(*data):
-        #    print(i[0])
-        #    self.username = i[0][0]
-        #    self.email = i[0][0]
-        #    self.ipaddress = i[0][0]
-        #    print(i[0][0])
-    #        break
+        Example:
+        -----------------------------------------------------
+            tito = User('Tito', '192.567.32' ,'tito@cat.com')
+            jiji = User('Jiji', '97.322.345', 'jiji@cat.com')
+            instance1 = Deliver(tito)
+            packet = 'Hello how are you'
+       Output 
+            >> Tito sent -- Hello how are you -- to jiji 
+    """
+    def __init__(self,user):
+         self.name= user.name
+         self.ipaddr= user.ipaddr
+         self.email= user.email
 
-#TODO: Create class for bob to delete or read packets
-Alice = GenPackets()
-#Alice.unpack_data([("jiji", "tito"), ("jiji@cats.com", "tito@cats.com"), ("182.784.986", "00000")])
-Alice.unpack_data([("jiji", "tito"), ("jiji@cats.com", "tito@cats.com"), ("182.784.986", "00000")])
-print(Alice)
-#Alice.unpack_data([("jiji", "XXXXX"), ("jiji@cats.com", "XX@cats.com"), ("182.784.986", "XXX")])
-#print(Alice)
-#Alice.unpack_data([("jiji", "tito"), ("jiji@cats.com", "tito@cats.com"), ("182.784.986", "00000")])
-#print(Alice)
+    def send(self, packet, user2):
+        print(f'{self.name} sent -- {packet} -- to {user2.name}')
+
+tito = User('Tito', '192.567.32' ,'tito@cat.com')
+jiji = User('Jiji', '97.322.345', 'jiji@cat.com')
+instance1 = Deliver(tito)
+packet = 'Hello how are you'
+instance1.send(packet, jiji)
